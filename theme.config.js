63ba079672e49f0e400dac4ad9dd8fb85cf8f731
Yuanlin Lin <im.yuanlinlin@gmail.com>
2022-11-28 09:17:01 +0800

    static open graph image

--- theme.config.js
+++ theme.config.js
@@ -30,11 +30,11 @@ export default {
       <meta name="description" content="Zeabur Docs" />
       <meta name="og:description" content="Zeabur: Deploy your service with one click." />
       <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:image" content="https://docs.zeabur.com/og.png" />
+      <meta name="twitter:image" content="/default-og.png" />
       <meta name="twitter:site:domain" content="docs.zeabur.com" />
       <meta name="twitter:url" content="https://docs.zeabur.com" />
       <meta name="og:title" content="Zeabur: Deploy your service with one click." />
-      <meta name="og:image" content="https://docs.zeabur.com/og.png" />
+      <meta name="og:image" content="/default-og.png" />
       <meta name="apple-mobile-web-app-title" content="Zeabur" />
       <link
         rel="apple-touch-icon"
