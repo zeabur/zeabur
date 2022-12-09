3ae92e43a4461a87cd34e917f2bc10e3b474942a
zzt <dyxzzt1108@163.com>
2022-12-09 22:51:30 +0800

    fix: server side route error

--- theme.config.js
+++ theme.config.js
@@ -1,5 +1,3 @@
-import Router from 'next/router';
-
 export default {
   project: {
     link: 'https://github.com/zeabur',
@@ -45,7 +43,6 @@ export default {
       <meta name="og:title" content="Zeabur: Deploy your service with one click." />
       <meta name="og:image" content="/default-og.png" />
       <meta name="apple-mobile-web-app-title" content="Zeabur" />
-      <meta name="og:url" content={`https://opengraph-dev.zeabur.app/api/og_image?url=${Router.route}`} />
       <link
         rel="apple-touch-icon"
         sizes="180x180"
