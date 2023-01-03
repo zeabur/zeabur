e71a166ff274bd766493c0519a10a51ef0574df2
Yuanlin Lin <im.yuanlinlin@gmail.com>
2023-01-03 17:18:06 +0800

    remove domain based localization because Nextra not support yet

--- next.config.js
+++ next.config.js
@@ -7,20 +7,6 @@ const withNextra = require('nextra')({
 module.exports = withNextra({
   i18n: {
     locales: ['en-US', 'zh-TW', 'zh-CN'],
-    defaultLocale: 'en-US',
-    domains: [
-      {
-        domain: 'docs.zeabur.com',
-        defaultLocale: 'en-US',
-      },
-      {
-        domain: 'docs.zeabur.tw',
-        defaultLocale: 'zh-TW',
-      },
-      {
-        domain: 'docs.zeabur.cn',
-        defaultLocale: 'zh-CN',
-      },
-    ],
+    defaultLocale: 'en-US'
   },
 })
