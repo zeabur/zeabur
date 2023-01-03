78d91bc3250fe31e325549d5f83d770838ee48f0
Yuanlin Lin <im.yuanlinlin@gmail.com>
2023-01-03 17:13:02 +0800

    domain based localization

--- next.config.js
+++ next.config.js
@@ -8,5 +8,19 @@ module.exports = withNextra({
   i18n: {
     locales: ['en-US', 'zh-TW', 'zh-CN'],
     defaultLocale: 'en-US',
+    domains: [
+      {
+        domain: 'zeabur.com',
+        defaultLocale: 'en-US',
+      },
+      {
+        domain: 'zeabur.tw',
+        defaultLocale: 'zh-TW',
+      },
+      {
+        domain: 'zeabur.cn',
+        defaultLocale: 'zh-CN',
+      },
+    ],
   },
 })
