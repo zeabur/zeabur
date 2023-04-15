1f32c01efef67edd3a6dd6323186707e263ed163
michaelyuhe <2312744987@qq.com>
2023-04-15 16:13:57 +0800

    try to fix twitter image

--- theme.config.js
+++ theme.config.js
@@ -34,53 +34,53 @@ export default {
       className="white-logo"
     />
   </>,
-  head:()=> {
+  head: () => {
     const { asPath } = useRouter();
     const { frontMatter } = useConfig();
     const ogEndpoint = 'https://opengraph-dev.zeabur.app/api/og';
     const ogUrl = `${ogEndpoint}?title=${frontMatter.ogImageTitle}&desc=${frontMatter.ogImageSubtitle}`
     return <>
-        <meta name="msapplication-TileColor" content="#ffffff" />
-        <meta name="theme-color" content="#ffffff" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta httpEquiv="Content-Language" content="en" />
-        <meta name="description" content="Zeabur Docs" />
+      <meta name="msapplication-TileColor" content="#ffffff" />
+      <meta name="theme-color" content="#ffffff" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta httpEquiv="Content-Language" content="en" />
+      <meta name="description" content="Zeabur Docs" />
 
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:site:domain" content="docs.zeabur.com" />
-        <meta name="twitter:url" content={`https://docs.zeabur.com${asPath}`} />
-        <meta name="twitter:image" content={ogUrl} />
+      <meta property="twitter:card" content="summary_large_image" />
+      <meta property="twitter:site:domain" content="docs.zeabur.com" />
+      <meta property="twitter:url" content={`https://docs.zeabur.com${asPath}`} />
+      <meta property="twitter:image" content={ogUrl} />
 
-        <meta name="og:title" content="Zeabur: Deploy your service with one click." />
-        <meta name="og:description" content="Zeabur: Deploy your service with one click." />
-        <meta name="og:url" content={`https://docs.zeabur.com${asPath}`} />
-        <meta name="og:image" content={ogUrl} />
+      <meta name="og:title" content="Zeabur: Deploy your service with one click." />
+      <meta name="og:description" content="Zeabur: Deploy your service with one click." />
+      <meta name="og:url" content={`https://docs.zeabur.com${asPath}`} />
+      <meta name="og:image" content={ogUrl} />
 
-        <meta name="apple-mobile-web-app-title" content="Zeabur" />
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/apple-touch-icon.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="192x192"
-          href="/android-chrome-192x192.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/favicon-32x32.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/favicon-16x16.png"
-        />
-      </>;
+      <meta name="apple-mobile-web-app-title" content="Zeabur" />
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/apple-touch-icon.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="192x192"
+        href="/android-chrome-192x192.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/favicon-16x16.png"
+      />
+    </>;
   },
   footer: {
     text: <span>
