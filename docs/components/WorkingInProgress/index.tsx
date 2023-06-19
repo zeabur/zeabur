b4bb727e4a4796f3c6a6965e6220e52c9a479107
Yi-Jyun Pan <yjpan@zeabur.com>
2023-06-19 18:20:25 +0800

    feat: Add WorkingInProgress component

---
+++ docs/components/WorkingInProgress/index.tsx
@@ -0,0 +1,22 @@
+import { useRouter } from 'next/router';
+
+// To make Nextra's custom MDX component style work, we have
+// to move what we want to render into a MDX file – which
+// made the codebase a bit messy :(
+import zh_TW from './zh_TW.mdx';
+import zh_CN from './zh_CN.mdx';
+import en_US from './en_US.mdx';
+
+export default function WorkingInProgress() {
+    const router = useRouter();
+
+    switch (router.locale) {
+        case 'zh-TW':
+            return zh_TW();
+        case 'zh-CN':
+            return zh_CN();
+        case 'en-US':
+        default:
+            return en_US();
+    }
+}
