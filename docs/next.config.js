efe6902c10ad2abe0d8204980d9c58094b1ae602
michaelyuhe <2312744987@qq.com>
2023-11-30 16:55:32 +0800

    wip: add Japanese in config

--- docs/next.config.js
+++ docs/next.config.js
@@ -2,13 +2,13 @@ const withNextra = require('nextra')({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.js',
   unstable_staticImage: true,
-  defaultShowCopyCode: true
-});
+  defaultShowCopyCode: true,
+})
 
 module.exports = withNextra({
   i18n: {
-    locales: ['en-US', 'zh-TW', 'zh-CN'],
-    defaultLocale: 'en-US'
+    locales: ['en-US', 'zh-TW', 'zh-CN', 'jp'],
+    defaultLocale: 'en-US',
   },
   basePath: '/docs',
   async redirects() {
--- docs/theme.config.js
+++ docs/theme.config.js
@@ -26,85 +26,96 @@ export default {
     { locale: 'en-US', text: 'English' },
     { locale: 'zh-TW', text: '繁體中文' },
     { locale: 'zh-CN', text: '简体中文' },
+    { locale: 'jp', text: '日本語' },
   ],
-  logo: <>
-    <img
-      src={LogoBlack.src}
-      style={{ height: 20, objectFit: 'contain' }}
-      alt="zeabur"
-      className="black-logo"
-    />
-    <img
-      src={LogoWhite.src}
-      style={{ height: 20, objectFit: 'contain' }}
-      alt="zeabur"
-      className="white-logo"
-    />
-  </>,
+  logo: (
+    <>
+      <img
+        src={LogoBlack.src}
+        style={{ height: 20, objectFit: 'contain' }}
+        alt="zeabur"
+        className="black-logo"
+      />
+      <img
+        src={LogoWhite.src}
+        style={{ height: 20, objectFit: 'contain' }}
+        alt="zeabur"
+        className="white-logo"
+      />
+    </>
+  ),
   useNextSeoProps() {
     return {
       titleTemplate: '%s – Zeabur',
     }
   },
   head: () => {
-    const r = useRouter();
-    const p = r.basePath + (r.locale === 'en-US' ? '' : '/' + r.locale) + r.asPath.replace('.'+r.locale, '')
-    const { frontMatter } = useConfig();
+    const r = useRouter()
+    const p =
+      r.basePath +
+      (r.locale === 'en-US' ? '' : '/' + r.locale) +
+      r.asPath.replace('.' + r.locale, '')
+    const { frontMatter } = useConfig()
     const ogEndpoint = 'https://og.zeabur.com/api/og'
     const ogQueryString = `title=${frontMatter.ogImageTitle}&desc=${frontMatter.ogImageSubtitle}`
     const encoded = base64Encode(ogQueryString)
     const ogUrl = `${ogEndpoint}/${encodeURIComponent(encoded)}.png`
 
-    const title = frontMatter?.ogImageTitle + " - Zeabur"
-    const description = frontMatter?.description || frontMatter.ogImageSubtitle || "Zeabur: Deploy your service with one click."
+    const title = frontMatter?.ogImageTitle + ' - Zeabur'
+    const description =
+      frontMatter?.description ||
+      frontMatter.ogImageSubtitle ||
+      'Zeabur: Deploy your service with one click.'
 
-    return <>
-      <meta name="msapplication-TileColor" content="#ffffff" />
-      <meta name="theme-color" content="#ffffff" />
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <meta httpEquiv="Content-Language" content="en" />
+    return (
+      <>
+        <meta name="msapplication-TileColor" content="#ffffff" />
+        <meta name="theme-color" content="#ffffff" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta httpEquiv="Content-Language" content="en" />
 
-      <title>{title}</title>
-      <meta property="og:title" content={title} />
+        <title>{title}</title>
+        <meta property="og:title" content={title} />
 
-      <meta name="description" content={description} />
-      <meta property="og:description" content={description} />
+        <meta name="description" content={description} />
+        <meta property="og:description" content={description} />
 
-      <meta property="twitter:card" content="summary_large_image" />
-      <meta property="twitter:site:domain" content="zeabur.com" />
-      <meta property="twitter:url" content={`https://zeabur.com${p}`} />
-      <meta property="twitter:image" content={ogUrl} />
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:site:domain" content="zeabur.com" />
+        <meta property="twitter:url" content={`https://zeabur.com${p}`} />
+        <meta property="twitter:image" content={ogUrl} />
 
-      <meta property="og:url" content={`https://zeabur.com${p}`} />
-      <meta property="og:image" content={ogUrl} />
+        <meta property="og:url" content={`https://zeabur.com${p}`} />
+        <meta property="og:image" content={ogUrl} />
 
-      <meta property="conaonical" content={`https://zeabur.com${p}`} />
+        <meta property="conaonical" content={`https://zeabur.com${p}`} />
 
-      <meta name="apple-mobile-web-app-title" content="Zeabur" />
-      <link
-        rel="apple-touch-icon"
-        sizes="180x180"
-        href="/apple-touch-icon.png"
-      />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="192x192"
-        href="/android-chrome-192x192.png"
-      />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="32x32"
-        href="/favicon-32x32.png"
-      />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="16x16"
-        href="/favicon-16x16.png"
-      />
-    </>;
+        <meta name="apple-mobile-web-app-title" content="Zeabur" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="192x192"
+          href="/android-chrome-192x192.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+      </>
+    )
   },
   footer: {
     text: (
