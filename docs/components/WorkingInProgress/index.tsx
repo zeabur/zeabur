ea945b4693246bc263c7c6fd4e256ba51a0e6061
michaelyuhe <2312744987@qq.com>
2023-12-11 13:16:03 +0800

    wip: add WIP component for Japanese

--- docs/components/WorkingInProgress/index.tsx
+++ docs/components/WorkingInProgress/index.tsx
@@ -1,22 +1,25 @@
-import { useRouter } from 'next/router';
+import { useRouter } from 'next/router'
 
 // To make Nextra's custom MDX component style work, we have
 // to move what we want to render into a MDX file – which
 // made the codebase a bit messy :(
-import zh_TW from './zh_TW.mdx';
-import zh_CN from './zh_CN.mdx';
-import en_US from './en_US.mdx';
+import jp from './jp.mdx'
+import zh_TW from './zh_TW.mdx'
+import zh_CN from './zh_CN.mdx'
+import en_US from './en_US.mdx'
 
 export default function WorkingInProgress() {
-    const router = useRouter();
+  const router = useRouter()
 
-    switch (router.locale) {
-        case 'zh-TW':
-            return zh_TW();
-        case 'zh-CN':
-            return zh_CN();
-        case 'en-US':
-        default:
-            return en_US();
-    }
+  switch (router.locale) {
+    case 'jp':
+      return jp()
+    case 'zh-TW':
+      return zh_TW()
+    case 'zh-CN':
+      return zh_CN()
+    case 'en-US':
+    default:
+      return en_US()
+  }
 }
