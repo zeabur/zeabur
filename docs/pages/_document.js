dcbdcdd6a513a9481af27d753bbfaadc1a5cb1e9
michaelyuhe <2312744987@qq.com>
2024-01-17 13:20:07 +0800

    fix: duplicated og

--- docs/pages/_document.js
+++ docs/pages/_document.js
@@ -8,25 +8,6 @@ export default function Document() {
         <link rel="icon" href="/icon?<generated>" />
         <link rel="shortcut icon" href="/favicon.svg" type="image/svg+xml" />
         <link rel="apple-touch-icon" href="/favicon.svg" />
-        <meta name="og:title" content="Zeabur" />
-        <meta name="og:image" content="/default-og.png" />
-
-        {/* Twitter Card */}
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:url" content="https://zeabur.com" />
-        <meta name="twitter:site" content="@zeaburapp" />
-        <meta
-          name="twitter:title"
-          content="Zeabur - Deploy Painlessly, Scale Infinitely"
-        />
-        <meta
-          name="twitter:description"
-          content="Zeabur is a platform helping deploy services painlessly and scale infinitely."
-        />
-        <meta
-          name="twitter:image"
-          content="https://zeabur.com/default-og.png"
-        />
 
         <script
           async
