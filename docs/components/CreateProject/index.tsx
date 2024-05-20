1b6e98be9ef0edd399fe0256977941a3f23e1378
michaelyuhe <2312744987@qq.com>
2024-05-20 11:31:39 +0800

    create project component

---
+++ docs/components/CreateProject/index.tsx
@@ -0,0 +1,23 @@
+import { useRouter } from 'next/router'
+
+import en_US from './en_US.mdx'
+import zh_CN from './zh_CN.mdx'
+import zh_TW from './zh_TW.mdx'
+import ja_JP from './ja_JP.mdx'
+
+export default function CreateProject() {
+  const router = useRouter()
+
+  switch (router.locale) {
+    case 'en-US':
+      return en_US()
+    case 'zh-CN':
+      return zh_CN()
+    case 'zh-TW':
+      return zh_TW()
+    case 'ja-JP':
+      return ja_JP()
+    default:
+      return en_US()
+  }
+}
