d80a4bbc02aba3221115d27c108ae1379d3da068
pan93412 <yjpan@zeabur.com>
2024-08-20 15:05:18 +0800

    Improve Nextra performance (#508)
    * chore: Upgrade docs dependencies
    
    The resource usage has decreased. Additionally, I did not observe any breaking changes (e.g., the og-image or content itself).
    
    * refactor: Rename _app.tsx to _app.mdx
    
    It improves the build speed and the size of HTML.

--- docs/pages/_app.tsx
+++
@@ -1,7 +0,0 @@
-import 'nextra-theme-docs/style.css';
-import './override.css';
-import type { AppProps } from 'next/app';
-
-export default function Nextra({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
-}
