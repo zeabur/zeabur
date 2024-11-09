1fb12248f7fc31ee00a3d19509b591bb4d2f17cf
Yen Cheng Lin <92412722+ridemountainpig@users.noreply.github.com>
2024-11-09 13:11:49 +0800

    feat(docs): add sitemap.xml to middleware config (#537)

--- docs/middleware.ts
+++ docs/middleware.ts
@@ -3,6 +3,6 @@ export { middleware } from 'nextra/locales'
 export const config = {
   matcher: [
     '/',
-    '/((?!api|_next/static|_next/image|favicon.ico|icon.svg|apple-icon.png|manifest).*)'
+    '/((?!api|_next/static|_next/image|favicon.ico|icon.svg|apple-icon.png|manifest|sitemap.xml).*)'
   ]
 }
