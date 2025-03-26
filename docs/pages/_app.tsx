68153f68e4f56a993184d0cc44b01b7683f11195
XIN <60823033+anxndsgn@users.noreply.github.com>
2025-03-26 17:53:13 +0800

    feat(locale): synchronize locale with localStorage on change

--- docs/pages/_app.tsx
+++ docs/pages/_app.tsx
@@ -1,5 +1,15 @@
+import { useEffect } from 'react'
+import { useRouter } from 'nextra/hooks'
+import { LOCALE_LOCAL_STORAGE_KEY } from 'docs/i18n-config'
 import './styles.css'
 
 export default function Nextra({ Component, pageProps }) {
+  const router = useRouter()
+  const { locale } = router
+
+  // sync locale to localStorage when it changes
+  useEffect(() => {
+    window.localStorage.setItem(LOCALE_LOCAL_STORAGE_KEY, locale)
+  }, [locale])
   return <Component {...pageProps} />
 }
