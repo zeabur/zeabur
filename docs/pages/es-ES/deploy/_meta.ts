9414fafbfc6ac488b5bce1059f87c6123173775a
Yi-Jyun Pan <yjpan@zeabur.com>
2025-06-05 12:21:15 +0800

    docs: update customize-prebuilt documentation (#604)

--- docs/pages/es-ES/deploy/_meta.ts
+++ docs/pages/es-ES/deploy/_meta.ts
@@ -3,7 +3,7 @@ export default {
   "watch-paths": "Rutas de Vigilancia",
   "dockerfile": "Implementar con Dockerfile",
   "deploy-button": "Botón de Implementación",
-  "customize-prebuilt": "Personalizar Preconstruido",
+  "customize-prebuilt": "Personalizar imagen Docker",
   "github": "Integración con GitHub",
   "special-variables": "Variables Especiales",
   "deploy-in-cli": "Implementar en CLI",
