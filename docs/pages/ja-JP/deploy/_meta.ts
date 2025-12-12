907b003d81056c9eae524caacec9247cdcd860e5
brian890809 <brian08090809@gmail.com>
2025-12-12 23:48:10 +0800

    feat: added create-service page

--- docs/pages/ja-JP/deploy/_meta.ts
+++ docs/pages/ja-JP/deploy/_meta.ts
@@ -1,5 +1,6 @@
 export default {
     "create-project": "プロジェクトを作成する",
+    "create-service": "サービスの作成",
     "root-directory": "ルートディレクトリ",
     "watch-paths": "監視パス",
     "dockerfile": "Dockerfile でデプロイ",
