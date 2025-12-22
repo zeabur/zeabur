eb84148ac647267b1e6a1e74fa6dc5ed5049dc86
brian890809 <brian08090809@gmail.com>
2025-12-22 20:14:05 +0800

    feat: writing websocket connection guide

--- docs/pages/en-US/developer/_meta.ts
+++ docs/pages/en-US/developer/_meta.ts
@@ -1,4 +1,5 @@
 export default {
   "use-api-key": "Create and Use API Keys",
-  "public-api": "Public API"
+  "public-api": "Public API",
+  "websocket-guide": "WebSocket Connection Guide"
 }
--- docs/pages/zh-CN/developer/_meta.ts
+++ docs/pages/zh-CN/developer/_meta.ts
@@ -1,4 +1,5 @@
 export default {
   "use-api-key": "创建和使用 API 密钥", 
-  "public-api": "开放 API"
+  "public-api": "开放 API",
+  "websocket-guide": "WebSocket 连接指南"
 }
