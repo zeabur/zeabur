d43a410e72c307679341b7d127fca138e133c632
IveTian <ivetien@outlook.com>
2026-03-30 16:22:52 +0800

    feat(domain): add domain docs

---
+++ docs/pages/en-US/domain/_meta.ts
@@ -0,0 +1,6 @@
+export default {
+  register: 'Register a Domain',
+  management: 'Domain Management',
+  dns: 'DNS Records',
+  profiles: 'Registrant Profiles',
+}
---
+++ docs/pages/es-ES/domain/_meta.ts
@@ -0,0 +1,6 @@
+export default {
+  register: 'Registrar un Dominio',
+  management: 'Gestión de Dominios',
+  dns: 'Registros DNS',
+  profiles: 'Perfiles de Registrante',
+}
