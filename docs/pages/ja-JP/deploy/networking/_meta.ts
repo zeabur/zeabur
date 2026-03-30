7a4526af67fcc3ddcffaf2c074a6d9c200c6baed
IveTian <ivetien@outlook.com>
2026-03-30 16:45:18 +0800

    fix(docs): fix domain doc error

---
+++ docs/pages/ja-JP/deploy/networking/_meta.ts
@@ -0,0 +1,7 @@
+export default {
+  index: { display: 'hidden' },
+  'public-networking': 'パブリックネットワーキング',
+  'private-networking': 'プライベートネットワーキング',
+  'domain-registration': 'ドメイン登録',
+  'high-availability': '高可用性',
+}
